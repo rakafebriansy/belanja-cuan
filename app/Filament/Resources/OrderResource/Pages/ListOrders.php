0277375933ae4d923f99d878edf78ffe8d48de15
rakafebriansy <kerenr445@gmail.com>
2024-12-15 21:38:55 +0700

    feat: admin panel - stats and tab group filter in order

--- app/Filament/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Resources/OrderResource/Pages/ListOrders.php
@@ -3,7 +3,9 @@
 namespace App\Filament\Resources\OrderResource\Pages;
 
 use App\Filament\Resources\OrderResource;
+use App\Filament\Resources\OrderResource\Widgets\OrderStats;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
 
 class ListOrders extends ListRecords
@@ -14,6 +16,25 @@ class ListOrders extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    protected function getHeaderWidgets(): array // function untuk menampilkan widget pada header list
+    {
+        return [
+            OrderStats::class
+        ]; // menampilkan order stats widgets
+    }
+
+    public function getTabs(): array // function untuk menampilkan widget pada header list
+    {
+        return [
+            null => Tab::make('All'),
+            'new' => Tab::make()->query(fn ($query) => $query->where('status','ordered')),
+            'processing' => Tab::make()->query(fn ($query) => $query->where('status','processing')),
+            'shipped' => Tab::make()->query(fn ($query) => $query->where('status','shipped')),
+            'delivered' => Tab::make()->query(fn ($query) => $query->where('status','delivered')),
+            'canceled' => Tab::make()->query(fn ($query) => $query->where('status','canceled')),
         ];
     }
 }
