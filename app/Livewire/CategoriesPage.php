f02f928d0af08fb18458c9462b7c4306def67cef
rakafebriansy <kerenr445@gmail.com>
2024-12-20 11:50:19 +0700

    feat: customer panel - dynamic content in home and categories page

--- app/Livewire/CategoriesPage.php
+++ app/Livewire/CategoriesPage.php
@@ -2,12 +2,18 @@
 
 namespace App\Livewire;
 
+use App\Models\Category;
 use Livewire\Component;
+use Livewire\Attributes\Title;
 
+#[Title('Categories - Belanja Cuan')]
 class CategoriesPage extends Component
 {
     public function render()
     {
-        return view('livewire.categories-page');
+        $categories = Category::where('is_active',1)->get();
+        return view('livewire.categories-page',[
+            'categories' => $categories
+        ]);
     }
 }
