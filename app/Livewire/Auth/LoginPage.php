5ad800b1441aafe3c9aa1dc25010607f086fd62a
rakafebriansy <kerenr445@gmail.com>
2024-12-23 18:13:17 +0700

    feat: customer panel - register and login page

--- app/Livewire/Auth/LoginPage.php
+++ app/Livewire/Auth/LoginPage.php
@@ -3,9 +3,30 @@
 namespace App\Livewire\Auth;
 
 use Livewire\Component;
+use Livewire\Attributes\Title;
 
+#[Title('Login')]
 class LoginPage extends Component
 {
+    public $email;
+    public $password;
+    public function save()
+    {
+        $this->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+        if(!auth()->attempt([
+            'email' => $this->email,
+            'password' => $this->password,
+        ])) {
+            $this->addError('password', 'These credentials don\'t match our records.');
+            // session()->flash('error','Invalid Credentials.');
+            return;
+        }
+
+        return redirect()->intended();
+    }
     public function render()
     {
         return view('livewire.auth.login-page');
