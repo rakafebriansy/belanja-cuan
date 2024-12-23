79f03a1111ec40983a9adad9bee92f63b947efe5
rakafebriansy <kerenr445@gmail.com>
2024-12-23 15:00:45 +0700

    feat: customer panel - cart page

--- app/Livewire/CartPage.php
+++ app/Livewire/CartPage.php
@@ -2,10 +2,41 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartManagement;
+use App\Livewire\Partials\Navbar;
 use Livewire\Component;
+use Livewire\Attributes\Title;
 
+#[Title('Cart - Belanja Cuan')]
 class CartPage extends Component
 {
+    public $cart_items = [];
+    public $grand_total;
+
+    public function mount() 
+    {
+        $this->cart_items = CartManagement::getCartItemsFromCookie();
+        $this->grand_total = CartManagement::calculateGrandTotal($this->cart_items);
+    }
+
+    public function removeItem($product_id)
+    {
+        $this->cart_items = CartManagement::removeCartItem($product_id);
+        $this->grand_total = CartManagement::calculateGrandTotal($this->cart_items);
+        $this->dispatch('update-cart-count', total_count: count($this->cart_items))->to(Navbar::class);
+    }
+
+    public function increaseQty($product_id)
+    {
+        $this->cart_items = CartManagement::incrementQuantityToCartItems($product_id);
+        $this->grand_total = CartManagement::calculateGrandTotal($this->cart_items);
+    }
+    public function decreaseQty($product_id)
+    {
+        $this->cart_items = CartManagement::decrementQuantityToCartItems($product_id);
+        $this->grand_total = CartManagement::calculateGrandTotal($this->cart_items);
+    }
+
     public function render()
     {
         return view('livewire.cart-page');
