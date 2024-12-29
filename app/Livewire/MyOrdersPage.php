e3bf6ee1c034d46862815188f4330d8b933e2898
rakafebriansy <kerenr445@gmail.com>
2024-12-29 21:07:20 +0700

    feat: customer panel - my orders page

--- app/Livewire/MyOrdersPage.php
+++ app/Livewire/MyOrdersPage.php
@@ -2,12 +2,20 @@
 
 namespace App\Livewire;
 
+use App\Models\Order;
 use Livewire\Component;
+use Livewire\Attributes\Title;
+use Livewire\WithPagination;
 
+#[Title('My Orders - Belanja Cuan')]
 class MyOrdersPage extends Component
 {
+    use WithPagination;
     public function render()
     {
-        return view('livewire.my-orders-page');
+        $my_orders = Order::where('user_id',auth()->id())->latest()->paginate(5);
+        return view('livewire.my-orders-page',[
+            'orders'=> $my_orders
+        ]);
     }
 }
