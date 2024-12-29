f162e15105aee807074881f03cd97f1bbf4f8ddd
rakafebriansy <kerenr445@gmail.com>
2024-12-29 13:26:00 +0700

    feat: stripe payment

--- app/Livewire/CheckoutPage.php
+++ app/Livewire/CheckoutPage.php
@@ -3,8 +3,12 @@
 namespace App\Livewire;
 
 use App\Helpers\CartManagement;
+use App\Models\Address;
+use App\Models\Order;
 use Livewire\Component;
 use Livewire\Attributes\Title;
+use Stripe\Checkout\Session;
+use Stripe\Stripe;
 
 #[Title('Checkout')]
 class CheckoutPage extends Component
@@ -17,6 +21,13 @@ class CheckoutPage extends Component
     public $state;
     public $zip_code;
     public $payment_method;
+    public function mount() 
+    {
+        $cart_items = CartManagement::getCartItemsFromCookie();
+        if(count($cart_items) == 0) {
+            return redirect()->route('products');
+        }
+    }
 
     public function placeOrder() 
     {
@@ -30,6 +41,67 @@ class CheckoutPage extends Component
             'zip_code' => 'required',
             'payment_method' => 'required',
         ]);
+
+        $cart_items = CartManagement::getCartItemsFromCookie();
+        
+        $line_items = [];
+
+        foreach($cart_items as $item) {
+            $line_items[] = [
+                'price_data' => [
+                    'currency' => 'IDR',
+                    'unit_amount' => $item['unit_amount'] * 100,
+                    'product_data' => [
+                        'name' => $item['name']
+                    ]
+                ],
+                'quantity' => $item['quantity']
+            ];
+        }
+
+        $order = new Order();
+        $order->user_id = auth()->user()->id;
+        $order->grand_total = CartManagement::calculateGrandTotal($cart_items);
+        $order->payment_method = $this->payment_method;
+        $order->payment_status = 'pending';
+        $order->status = 'ordered';
+        $order->currency = 'idr';
+        $order->shipping_amount = 0;
+        $order->shipping_method = 'none';
+        $order->notes = 'Order placed by ' . auth()->user()->name;
+
+        $address = new Address();
+        $address->first_name = $this->first_name;
+        $address->last_name = $this->last_name;
+        $address->phone = $this->phone;
+        $address->street = $this->street;
+        $address->city = $this->city;
+        $address->state = $this->state;
+        $address->zip_code = $this->zip_code;
+        
+        $redirect_url = '';
+
+        if($this->payment_method == 'stripe') {
+            Stripe::setApiKey(env('STRIPE_SECRET'));
+            $session_checkout = Session::create([
+                'payment_method_types' => ['card'],
+                'mode' => 'payment',
+                'customer_email' => auth()->user()->email,
+                'line_items' => $line_items,
+                'success_url' => route('success') . '?session_id={CHECKOUT_SESSION_ID}',
+                'cancel_url' => route('cancel')
+            ]);
+
+            $redirect_url = $session_checkout->url;
+        } else {
+            $redirect_url = route('success');
+        }
+        $order->save();
+        $address->order_id = $order->id;
+        $address->save();
+        $order->orderDetails()->createMany($cart_items);
+        CartManagement::clearCartItemsFromCookie();
+        return redirect($redirect_url);
     }
     public function render()
     {
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',HomePage::class);
 Route::get('/categories',CategoriesPage::class);
-Route::get('/products',ProductsPage::class);
+Route::get('/products',ProductsPage::class)->name('products');
 Route::get('/cart',CartPage::class);
 Route::get('/products/{slug}',ProductDetailPage::class);
 
@@ -38,6 +38,6 @@ Route::middleware('auth')->group(function() {
     Route::get('/checkout',CheckoutPage::class);
     Route::get('/my-orders',MyOrdersPage::class);
     Route::get('/my-orders/{id}', MyOrderDetailPage::class);
-    Route::get('/success', SuccessPage::class);
-    Route::get('/cancel', CancelPage::class);
+    Route::get('/success', SuccessPage::class)->name('success');
+    Route::get('/cancel', CancelPage::class)->name('cancel');
 });
